61c18554a2e923ae5d6c788026c241adbb598082
Askiev <montagues@list.ru>
2022-09-11 23:38:30 +0300

    complete

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private final String fio;
+    private int department;
+    private double salary;
+    private final int id;
+    private static int counter = 1;
+
+    public Employee(String fio, int department, double salary) {
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int dept) {
+        department = dept;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "| " + getId() + "| " + getFio() + "| " + getDepartment() + "| " + getSalary() + "|";
+    }
 }
